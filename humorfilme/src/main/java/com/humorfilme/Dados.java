6943bda54ee2ad0582fe9ae9f1beb9309c1346d1
Raíssa Santos <39340728+raixasantos@users.noreply.github.com>
2022-05-22 22:53:47 -0300

    Adicionando ideias iniciais

---
+++ humorfilme/src/main/java/com/humorfilme/Dados.java
@@ -0,0 +1,14 @@
+package com.humorfilme;
+
+public class Dados {
+    private String searchType;
+    private String expression;
+
+    public String getSearchType() {
+        return searchType;
+    }
+
+    public String getExpression() {
+        return expression;
+    }
+}
---
+++ humorfilme/src/main/java/com/humorfilme/IMDB.java
@@ -0,0 +1,47 @@
+package com.humorfilme;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.logging.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+// import java.util.Arrays;
+import com.google.gson.Gson;
+
+public class IMDB {
+
+    public static void main(String[] args) {
+        try {
+            String url = "https://imdb-api.com/en/API/Search/k_cdmc38z1/inception%202010";
+
+            HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
+
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+
+            if (conn.getResponseCode() != 200) {
+                System.out.println("Erro " + conn.getResponseCode() + " ao obter dados da URL " + url);
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+
+            String output = "";
+            String line;
+            while ((line = br.readLine()) != null) {
+                output += line;
+            }
+
+            conn.disconnect();
+
+            Gson gson = new Gson();
+            Dados dados = gson.fromJson(new String(output.getBytes()), Dados.class);
+
+            System.out.println("SEARCH TYPE: " + dados.getSearchType());
+            System.out.println("EXPRESSION: " + dados.getExpression());
+
+        } catch (IOException ex) {
+            Logger.getLogger(IMDB.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
